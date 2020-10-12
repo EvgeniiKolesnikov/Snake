dbd06f56ad5ad2120efa9e358c39f5c584a263c9
EvgeniiKolesnikov <jekow@yandex.ru>
2020-10-12 17:00:19 +0300

    Inheritance

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+
+            Console.SetBufferSize(80, 25);
+            // Draw Border
+            HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            topLine.DrawLine();
+            bottomLine.DrawLine();
+            leftLine.DrawLine();
+            rightLine.DrawLine();
+
             Point p1 = new Point(1, 3, '*', true);
             Point p2 = new Point(4, 5, '#', true);
             Console.WriteLine();

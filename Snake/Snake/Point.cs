e15cdf63237c4656873f7060b3321476867be243
EvgeniiKolesnikov <jekow@yandex.ru>
2020-10-12 20:21:18 +0300

    Snail WASD

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -45,8 +45,8 @@ namespace Snake
         {
             if      (direction == Direction.RIGHT) x = x + offset;
             else if (direction == Direction.LEFT) x = x - offset;
-            else if (direction == Direction.UP)   y = y + offset;
-            else if (direction == Direction.DOWN) y = y - offset;
+            else if (direction == Direction.UP)   y = y - offset;
+            else if (direction == Direction.DOWN) y = y + offset;
         }
         public void Draw()
         {

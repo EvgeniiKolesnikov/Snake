4b5a3036cf5278b5fd834381932711dba6103b58
EvgeniiKolesnikov <jekow@yandex.ru>
2020-10-12 11:36:46 +0300

    RAM

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,6 +15,21 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+            Console.WriteLine();
+            Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}"); // 1 3
+            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}"); // 4 5
+
+            p1 = p2;
+            Console.WriteLine($"p1 = p2");
+
+            Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}"); // 4 5
+            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}"); // 4 5
+
+            p2.x++;
+            p2.y++;
+            Console.WriteLine($"p2++");
+            Console.WriteLine($"p1 = {p1.x}, {p1.y}, {p1.sym}"); // 5 6
+            Console.WriteLine($"p2 = {p2.x}, {p2.y}, {p2.sym}"); // 5 6
 
             Console.ReadLine();
         }
